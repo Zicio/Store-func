f056ab10dff6487e288aa9198c9daa18448aa657
Alexander Burachkov <zicio@yandex.ru>
2022-04-23 15:26:20 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="container">
       <div className="block">
         <div className="image-box">
-          <img className="photo" src="/image.jpeg" alt="фото товара" />
+          <img className="photo" src="../image.jpeg" alt="фото товара" />
         </div>
         <div className="window">
           <ShopItemFunc item={itemModel} />
